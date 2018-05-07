517b994968eb60066e748a7e71828945c21e46f6
nirb <lukass77@gmail.com>
2018-05-08 00:58:05 +0300

    react js project

---
+++ reactapp/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root1'));
+ReactDOM.render(<App />, document.getElementById('root2'));
+ReactDOM.render(<App />, document.getElementById('root3'));
