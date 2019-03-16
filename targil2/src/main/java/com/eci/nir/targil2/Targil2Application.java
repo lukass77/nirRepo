c37b818e9e40dbcfa0698e99352af0d431963732
nirb <lukass77@gmail.com>
2019-03-16 12:41:04 +0200

    tragil 2 first

---
+++ targil2/src/main/java/com/eci/nir/targil2/Targil2Application.java
@@ -0,0 +1,13 @@
+package com.eci.nir.targil2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Targil2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Targil2Application.class, args);
+	}
+
+}
